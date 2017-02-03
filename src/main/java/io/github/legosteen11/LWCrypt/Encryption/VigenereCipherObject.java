1158c047cc6fafc9331e5f3d74eff1f431059c8f
Wouter <wouterdoeland@gmail.com>
2017-02-03 21:18:08 +0100

    Added some logic to vigenere to eventually decrypt it using letter frequencies.

--- src/main/java/io/github/legosteen11/LWCrypt/Encryption/VigenereCipherObject.java
+++ src/main/java/io/github/legosteen11/LWCrypt/Encryption/VigenereCipherObject.java
@@ -3,6 +3,8 @@ package io.github.legosteen11.LWCrypt.Encryption;
 import io.github.legosteen11.LWCrypt.Util.CharUtils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by wouter on 2-2-17.
@@ -63,6 +65,53 @@ public class VigenereCipherObject {
         this.cipherText = cipherTextBuilder.toString();
 
         return this.cipherText;
+    }
+    
+    public static String[] stringToTriplets(String string) {
+        Set<String> stringsSet = new HashSet<>();
+        for(int i = 0; i < string.length() - 2; i++) {
+            stringsSet.add(string.substring(i, i + 2));
+        }
+        return stringsSet.toArray(new String[0]);
+    }
+    
+    public static int[] distanceBetweenPositions(int[] positions) {
+        ArrayList<Integer> distances = new ArrayList<>();
+        for (int position :
+                positions) {
+            for (int i = 0; i < positions.length; i++) {
+                int difference = positions[i] - position;
+                
+                if(difference <= 0) continue;
+                
+                distances.add(difference);
+            }
+        }
+        
+        return distances.stream().mapToInt(i->i).toArray();
+    }
+    
+    public static int[] positionsInString(String match, String string) {
+        ArrayList<Integer> distanceList = new ArrayList<>();
+        String remainingString = string;
+        int distanceStrippedAway = 0;
+        while (remainingString.contains(match)) {
+            int distanceToStrip = remainingString.indexOf(match);
+            distanceList.add(distanceStrippedAway + distanceToStrip);
+            distanceToStrip += match.length();
+            distanceStrippedAway += distanceToStrip;
+            remainingString = remainingString.substring(distanceToStrip, remainingString.length());
+        }
+        return distanceList.stream().mapToInt(i->i).toArray();
+    }
+    
+    public static int[] possibleKeySizes(int distanceBetweenPositions) {
+        ArrayList<Integer> possibleKeySizes = new ArrayList<>();
+        for(double i = 1; i <= distanceBetweenPositions; i++) {
+            if(distanceBetweenPositions / i % 1 == 0) possibleKeySizes.add((int) i);
+        }
+        
+        return possibleKeySizes.stream().mapToInt(i->i).toArray();
     }
     
     public String getCipherText() {
