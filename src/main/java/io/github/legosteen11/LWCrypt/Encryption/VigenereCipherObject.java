8afe718a5a0603dea6d0a6455db32abd27e3188a
Wouter <wouterdoeland@gmail.com>
2017-02-04 14:01:42 +0100

    Now able to decrypt Vigenere :D

--- src/main/java/io/github/legosteen11/LWCrypt/Encryption/VigenereCipherObject.java
+++ src/main/java/io/github/legosteen11/LWCrypt/Encryption/VigenereCipherObject.java
@@ -1,13 +1,16 @@
 package io.github.legosteen11.LWCrypt.Encryption;
 
 import io.github.legosteen11.LWCrypt.Util.CharUtils;
+import io.github.legosteen11.LWCrypt.Util.Modulo;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by wouter on 2-2-17.
  */
 public class VigenereCipherObject {
+    public static final HashMap<Character, Double> NL_LETTER_FREQUENCY = nlLetterFrequencyMap();
     private String cipherText;
     private String plainText;
     private String key;
@@ -167,11 +170,92 @@ public class VigenereCipherObject {
         }
         return keySizes[0];
     }
+    
+    public static double getFrequencyDelta(HashMap<Character, Double> charsWithFrequencies, HashMap<Character, Double> correctFrequencies) {
+        double delta = 0;
+        for (int i = 0; i < 26; i++) {
+            delta += Math.abs((double) charsWithFrequencies.get(CharUtils.Mod26ToChar(i)) - correctFrequencies.get(CharUtils.Mod26ToChar(i)));
+        }
+        return delta;
+    }
+    
+    public static HashMap<Character, Double> createFrequencies(HashMap<Character, Integer> characterIntegerHashMap, int mapSizeTotal) {
+        HashMap<Character, Double> frequencyMap = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            int charAmount = characterIntegerHashMap.getOrDefault(CharUtils.Mod26ToChar(i), 0);
+            double frequency =  (charAmount / (double) mapSizeTotal) * 100;
+            frequencyMap.put(CharUtils.Mod26ToChar(i), frequency);
+        }
+        return frequencyMap;
+    }
+    
+    public static HashMap<Character, Integer> charAmounts(String string) {
+        HashMap<Character, Integer> charAmountMap = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            char character = string.charAt(i);
+            Integer value = charAmountMap.get(new Character(character));
+            if (value == null) {
+                charAmountMap.put(character, 1);
+            } else {
+                charAmountMap.put(character, value + 1);
+            }
+        }
+        return charAmountMap;
+    }
+    
+    public static char mostLikelyKey(HashMap<Character, Integer> characterIntegerHashMap, HashMap<Character, Double> correctFrequencies, int totalSize) {
+        HashMap<Character, Double> frequencyDeltaForEachKey = new HashMap<>();
+        char mostLikelyKey = 'a';
+        double delta = Integer.MAX_VALUE;
+        for (int i = 0; i < 26; i++) {
+            HashMap<Character, Integer> currentCharAmounts = new HashMap<>();
+            int totalChars = 0;
+            for (int j = 0; j < 26; j++) {
+                int amount = characterIntegerHashMap.getOrDefault(CharUtils.Mod26ToChar(Modulo.performModulo(new Integer(j + i), 26)), 0);
+                currentCharAmounts.put(CharUtils.Mod26ToChar(j), amount);
+                totalChars+=amount;
+            }
+            double frequencyDelta = getFrequencyDelta(createFrequencies(currentCharAmounts, totalChars), correctFrequencies);
+            if(frequencyDelta < delta) {
+                delta = frequencyDelta;
+                mostLikelyKey = CharUtils.Mod26ToChar(i);
+            }
+            frequencyDeltaForEachKey.put(CharUtils.Mod26ToChar(i), frequencyDelta);
+        }
+        
+        return mostLikelyKey;
+    }
+    
+    public static Decrypted crack(String cipherText, HashMap<Character, Double> correctFrequencies) {
+        cipherText = cipherText.toLowerCase();
+        int mostLikelyKeySize = mostLikelyKeySize(cipherText);
+        ArrayList<HashMap<Character, Integer>> charArrayWithamount = new ArrayList<>();
+        ArrayList<Character> mostLikelyCharacters = new ArrayList<>();
+        String key = "";
+        for(int i = 0; i < mostLikelyKeySize; i++) {
+            ArrayList<Character> currentChars = new ArrayList<>();
+            for (int j = i; j < cipherText.length(); j+= mostLikelyKeySize) {
+                currentChars.add(cipherText.charAt(j));
+            }
+            HashMap<Character, Integer> charAmounts = charAmounts(currentChars.stream().map(Object::toString).collect(Collectors.joining()));
+            charArrayWithamount.add(charAmounts);
+            key += mostLikelyKey(charAmounts, correctFrequencies, cipherText.length());
+        }
+        VigenereCipherObject vigenereCipherObject = new VigenereCipherObject(cipherText);
+        String plainText = vigenereCipherObject.decrypt(key);
+        return new Decrypted("vigenere", cipherText, plainText, key);
+    }
 
     public static void main(String[] args) {
         String string = "nittoriurtikferkveuloldeotewxbekdwedqoevoslwetedwaajnevjkagacofvoslwetedqoevqengogikgieryudsdwelonicxielsniwneryovad";
         String string2 = "duxswwwqymiezvjxzslmexhdwysseueexnwqucvweepvmyszdtadhvnwooxbsajuwxzslmexkfwiusehesmjonsnfndmxnwysbzdnuikaswsuxrsimsq";
-        System.out.println("Most likely keysize = " + mostLikelyKeySize(string2));
+        String voorbeeld = "BVVZQGDWIPIUIXQGFVIUCUQKHTVTTDXVUXITVTMZVVVDUYEPMQGZAIOWKMVZDXOIQCSMAUYJDSAGNMPTTVNXPTTPMZTJMAUHBCRPXXMCUIRVKLWRVKUCDQHPTCAEMCXMXADELHMIVZIQCXMXMAUSPQXEMVPPEHIHTEBMSBZTNATEQWIPRZZADIOIXSKLEFTIWRQXELMSKVMPBPIMRBGZMQSTKIPXTEHMVCMIRPTMWVYEVVTBALAHUCDQHPTCAMEPRVKQIFWRPSRBIDTVVKQIRTHYTKLILTVQKQCJKLMEDWIFOZRRPPKSPQXEMVAUXMPUYBQWMPEBAQTYWRPTILDQHVVZQTIBMS";
+        System.out.println("Most likely keysize = " + mostLikelyKeySize(voorbeeld));
+        System.out.println("Cracking: ");
+        Decrypted result = crack(voorbeeld, NL_LETTER_FREQUENCY);
+        System.out.println("Key: " + result.getKey());
+        System.out.println("Ciphertext: " + result.getCipherText());
+        System.out.println("Plaintext: " + result.getPlainText());
     }
     
     public String getCipherText() {
@@ -184,5 +268,36 @@ public class VigenereCipherObject {
 
     public String getKey() {
         return key;
+    }
+    
+    private static HashMap<Character, Double> nlLetterFrequencyMap() { // Create a map with all the dutch letter frequencies.
+        HashMap<Character, Double> frequencies = new HashMap<>();
+        frequencies.put('a', 7.49);
+        frequencies.put('b', 1.58);
+        frequencies.put('c', 1.24);
+        frequencies.put('d', 5.93);
+        frequencies.put('e', 18.91);
+        frequencies.put('f', 0.81);
+        frequencies.put('g', 3.40);
+        frequencies.put('h', 2.38);
+        frequencies.put('i', 6.50);
+        frequencies.put('j', 1.46);
+        frequencies.put('k', 2.25);
+        frequencies.put('l', 3.57);
+        frequencies.put('m', 2.21);
+        frequencies.put('n', 10.03);
+        frequencies.put('o', 6.06);
+        frequencies.put('p', 1.57);
+        frequencies.put('q', 0.009);
+        frequencies.put('r', 6.41);
+        frequencies.put('s', 3.73);
+        frequencies.put('t', 6.79);
+        frequencies.put('u', 1.99);
+        frequencies.put('v', 2.85);
+        frequencies.put('w', 1.52);
+        frequencies.put('x', 0.04);
+        frequencies.put('y', 0.035);
+        frequencies.put('z', 1.39);
+        return frequencies;
     }
 }
